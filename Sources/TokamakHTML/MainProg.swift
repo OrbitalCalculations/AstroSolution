9984206171ac29c72c5d70a992eccb0abc8e7e95
wiggles <hpaelike@marum.de>
2021-12-03 18:12:40 +0100

    ..

--- Sources/TokamakHTML/MainProg.swift
+++ Sources/TokamakHTML/MainProg.swift
@@ -53,6 +53,7 @@ struct ContentView: View {
     #if os(WASI)
     let document = JSObject.global.document
     let script = document.createElement("script")
+    //let _ = script.setAttribute("src", "https://paloz.marum.de/AstroComputation/plotly-latest.min.js")
     let _ = script.setAttribute("src", "https://cdn.plot.ly/plotly-latest.min.js")
     let _ = document.head.appendChild(script)
     //_ = document.head.insertAdjacentHTML("beforeend", #"""
@@ -62,6 +63,7 @@ struct ContentView: View {
     //"""#)
     #endif
   }
+  
   
   var body: some View {
     let _ = setupPlotly()
@@ -93,27 +95,71 @@ struct ContentView: View {
         
         ComputationButton(solutionSelection: solutionProxy)
           .buttonStyle(BorderedProminentButtonStyle())
-        CustomDivider()
-        }
         
+        }
+        CustomDivider()
         
         Button("Plotly"){
           print("Plotly")
           let plotly = JSObject.global.Plotly.object!
-          let _ = plotly.react!("66154CE9-D203-4126-89F4-837930B5EF87", JSObject.global.JSON.object!.parse!(
-            PlotlySupport.plotlyConfig))
+          let (x,y) = pAModelling.pAPrediction(fgam: environment.fgam, cmar: environment.cmar)
           
-          let x = [1.0, 2.0, 3.0, 4.0]
-          let y = [10.0, 15.0, 13.0, 17.0]
-          let data: [Trace] = [
-              Scatter(name: "Scatter", x: x, y: y),
-              Bar(name: "Bar", x: x, y: y)
-          ]
-          let figure = Figure(data: data)
-          try! figure.show()
+          let trace1 = Scatter(
+            name: "general precession in longitude (pA)",
+            x: x,
+            y: y,
+            mode: .lines,
+            line: .init(
+                color: 0x9748a1,
+                width: 2
+            ),
+            xAxis: .init(
+                //uid: 1,
+                //mirror: .on,
+                //tickLength: 4,
+                //showTickLabels: false,
+                //tickFont: .init(size: 10),
+                showLine: true,
+                //showGrid: true,
+                //gridColor: 0xffffff,
+                //zeroLine: false,
+                domain: [-50000000.0, 0.0]
+            ),
+            yAxis: .init(
+                //uid: 1,
+                //mirror: .on,
+                //tickLength: 4,
+                //tickFont: .init(size: 10),
+                //hoverFormat: ".2f",
+                showLine: true,
+                //showGrid: true,
+                //gridColor: 0xffffff,
+                //zeroLine: false,
+                domain: [49.5, 53.5]
+            )
+          )
+          trace1.xAxis.anchor = .yAxis(trace1.yAxis)
+          trace1.yAxis.anchor = .xAxis(trace1.xAxis)
+
+          
+          let data: [Trace] = [trace1]
+          
+          let config = Config(staticPlot: true, responsive: true)
+          let layout = Layout(height: 250.0, /*margin: Layout.Margin.init(autoExpand:true),*/ paperBackgroundColor: .lightGray/*, plotBackgroundColor: .gray*//*, xAxis: [XAxis(name: "Age(years)", tick0: 0.0, dTick: 5000.0)], yAxis: [YAxis(name: "pA", tick0: 50.0, dTick: 0.5)]*/)
+          let figure = Figure(data: data, layout: layout, config: config)
+
+          let encoder = JSONEncoder()
+          guard let figureData = try? encoder.encode(figure),
+                let jsonFigureData = String(data: figureData, encoding: .utf8) else {
+                  print("cannot encode figure")
+                  return
+          }
+          
+          let _ = plotly.react!("66154CE9-D203-4126-89F4-837930B5EF87", JSObject.global.JSON.object!.parse!(
+          jsonFigureData))
           
         }
-        StatusView()
+        //StatusView()
         PlotlyView()
         
     }.environmentObject(environment)
--- Sources/TokamakHTML/Views/AppEnvironment.swift
+++ Sources/TokamakHTML/Views/AppEnvironment.swift
@@ -12,6 +12,7 @@ import OpenCombineShim
 import OpenCombineJS
 
 import Foundation
+import Plotly
 
 final class AppEnvironment: ObservableObject {
   @Published var solution: AstroSolutionEnum = .La1993
@@ -64,6 +65,49 @@ final class AppEnvironment: ObservableObject {
         
     let fileName =
     "\(solutionName)_(\(String(format: "%6.4f", fgam)),\(String(format:"%3.1f", cmar)))_0-\(startMyr)Ma.txt"
+    
+    //updatePlot()
+    
     return fileName
   }
+  
+  /*public func updatePlot(){
+    print("Plotly onAppear ")
+    return
+    let window = JSObject.global.document.defaultView.object!
+    
+    print("window = \(window)")
+
+    let eventListener = JSClosure.init( {_ in
+      print("all loaded")
+      
+      let plotly = JSObject.global.Plotly.object!
+      //let _ = plotly.react!("66154CE9-D203-4126-89F4-837930B5EF87", JSObject.global.JSON.object!.parse!(
+      //  PlotlySupport.plotlyConfig))
+      
+      let (x,y) = pAModelling.pAPrediction(fgam: self.fgam, cmar: self.cmar)
+      //let x = [1.0, 2.0, 3.0, 4.0]
+      //let y = [10.0, 15.0, 13.0, 17.0]
+      let data: [Trace] = [
+        Scatter(name: "pA", x: x, y: y, mode: .lines)//,
+      ]
+      
+      let config = Config(staticPlot: true, responsive: true)
+      let layout = Layout(height: 250.0, margin: Layout.Margin.init(autoExpand:true), paperBackgroundColor: .lightCyan/*, plotBackgroundColor: .gray*/)
+      let figure = Figure(data: data, layout: layout, config: config)
+
+      let encoder = JSONEncoder()
+      guard let figureData = try? encoder.encode(figure),
+            let jsonFigureData = String(data: figureData, encoding: .utf8) else {
+              print("cannot encode figure")
+              return JSValue.undefined
+      }
+      
+      let _ = plotly.react!("66154CE9-D203-4126-89F4-837930B5EF87", JSObject.global.JSON.object!.parse!(
+      jsonFigureData))
+      return JSValue.undefined
+    })
+    
+    window.addEventListener!(JSValue.object(eventListener))
+  }*/
 }
